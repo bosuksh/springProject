be4466403a0de20f100acba47526314172e0ccc6
이상협 <leesanghyup@isanghyeob-ui-MacBook-Pro.local>
2019-10-10 12:59:43 +0900

    API Create

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -2,34 +2,44 @@ package com.example.study.controller.api;
 
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import jdk.internal.org.jline.utils.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest,UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")        // /api/user
-    public Header create(@RequestBody ) {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
+        log.info("{}",userApiRequest);
+        return userApiLogicService.create(userApiRequest);
     }
 
     @Override
     @GetMapping("{id}")     // api/user/{id}
-    public Header read(@PathVariable(name = "id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")        // /api/user
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         return null;
     }
 
     @Override
     @DeleteMapping("{id}")          // /api/user/{id}
-    public Header delete(@PathVariable Long id) {
+    public Header<UserApiResponse> delete(@PathVariable Long id) {
         return null;
     }
 }
